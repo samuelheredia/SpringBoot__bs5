f95a8e3cf67118702c80483f01803b8dbb9cb738
Samuel Heredia <samuel.heredia@bosonit.com>
2021-10-20 10:43:24 +0200

    Initial commit

---
+++ src/main/java/com/bosonit/springboot/bs5/Bs5Application.java
@@ -0,0 +1,13 @@
+package com.bosonit.springboot.bs5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bs5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bs5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/bosonit/springboot/bs5/Bs5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bosonit.springboot.bs5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bs5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
